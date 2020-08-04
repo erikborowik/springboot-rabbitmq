a82240863dc88012e28bca406f1e2f16920984e2
Erik Borowik Rosa <erik.borowik@gmail.com>
2020-08-04 09:38:52 -0300

    Initial commit

---
+++ netflix-filmes/src/main/java/br/com/xpto/NetflixFilmesApplication.java
@@ -0,0 +1,13 @@
+package br.com.xpto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixFilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixFilmesApplication.class, args);
+	}
+
+}
